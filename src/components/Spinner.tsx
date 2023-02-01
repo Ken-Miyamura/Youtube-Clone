a89e12e62e8f3b22bba1bd512c5982b38593f36e
“Your <mykmiyamu@gmail.com>
2023-02-02 01:44:27 +0900

    読み込みマークのコンポーネントと動画カードコンポーネントのDOM作成とデータ入れ込み

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Spinner = () => {
 	return (
-    <div>Spinner</div>
+    <div className="flex item-center w-full justify-center py-3">
+			<div className="w-10 h-10 border-2 border-red-600 border-solid rounded-full animate-spin border-t-transparent">
+
+			</div>
+		</div>
   )
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect } from 'react'
 import InfiniteScroll from 'react-infinite-scroll-component';
+import Card from '../components/Card';
 import Navbar from '../components/Navbar'
 import Sidebar from '../components/Sidebar'
 import Spinner from '../components/Spinner';
+import { HomePageVideos } from '../interface';
 import { useAppDispatch, useAppSelector } from '../store/hook';
 import { getHomePageVideo } from '../store/reducers/getHomePageVideos';
 
@@ -21,7 +23,18 @@ const Home = () => {
 			<div className="flex" style={{ height: "92.5vh" }}>
 				<Sidebar />
 				{videos.length ? (
-					<InfiniteScroll>
+					<InfiniteScroll 
+						dataLength={videos.length} 
+						next={() => dispatch(getHomePageVideo(true))}
+						hasMore={videos.length < 500}
+						loader={<Spinner />}
+						height={650}
+					>
+						<div className="grid gap-y-14 gap-x-8 grid-cols-4 p-8">
+							{videos.map((item: HomePageVideos) => {
+								return <Card data={item} key={item.videoId} />
+							})}
+						</div>
 
 					</InfiniteScroll>
 				) : (
